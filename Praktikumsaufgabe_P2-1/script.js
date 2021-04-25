96eed48b85fadb5247bb47b3ea9333b97275481f
MonikaHo29 <hocamtumonika29@gmail.com>
2021-04-25 18:06:37 +0200

    Aufgabe bearbeitet

--- Praktikumsaufgabe_P2-1/script.js
+++ Praktikumsaufgabe_P2-1/script.js
@@ -1,7 +1,32 @@
 "use strict";
-/*Praktikumsaufgabe 2.1 Einführung TypeScript*/
-/* Aufgabe 1 - Basics*/
-/*  a)  Es wird "Alles Klar? Logo!" in der Konsole ausgegeben   */
+/* Praktikumsaufgabe 2.1 Einführung TypeScript */
+// Aufgabe 1 - Basics
+//  a)  In der Konsole wird "Alles Klar? Logo!" ausgegeben  
+/* function a$(): void {
+    let x.1: string = "Alles";
+    console.log(x);
+    func0();
+    console.log("Logo!");
+}
+
+a1();
+
+function func 0(): void {
+    console.log("Klar?");
+}
+*/
+/*  a)  Zulässige Variablennamen können Ziffer, Buchstaben oder Sonderzeichen haben. -> z.B. a$
+        
+        Nicht zulässige Variablennamen dürfen keine Leerzeichen, Bindestrich und Punkte haben, nicht mit einem Ziffer beginnen
+        oder als string, boolean, number, etc. heißen.
+
+    b)  Mit der Schaltfläche "Schritt darüber", wird der Debugger alle Zeile durchlaufen. Jedoch wird die innere Funktion nicht aufgerufen,
+        sondern springt zur nächste Funktion.
+
+        Mit der Schaltfläche "Schritt hinein", wird der Debugger alle Zeile nacheinander durchlaufen mit der innere Funktion.
+
+*/
+//c) 
 function a1() {
     let x = "Alles";
     console.log(x);
@@ -21,23 +46,12 @@ function func2() {
 function func3() {
     console.log("Logo!");
 }
-/*  a)  Zulässige Variablennamen sind können Ziffer, Buchstaben oder Sonderzeichen haben.
-        
-        Nicht zulässige Variablennamen dürfen nicht mit Ziffer beginnen, keine Leerzeichen oder Bindestrich haben
-        oder Schlüsselwörter wie string, boolean, number etc.
-
-     b) Mit dem Schalter "Schritt darüber" werden nur die Zeile von 4-7 nacheinander durchgeführt.
-        Jedoch überspringt es die Zeilen, indem sich die Funktion befindet
-        Mit dem Schalter "Schritt hinein" werden alle Zeilen durchlaufen. Jedoch nicht nach der Reihenfolge.
-        --> nach der Zeile 4 springt es in die Funktion a1 zur Zeile 11
-
-    c)  siehe Konsole im Browser
-*/
-/* Aufgabe 2 - Kontinuierliche Variablenmanipulation*/
+/* Aufgabe 2 - Kontinuierliche Variablenmanipulation */
 /*
-In der FUnktion a2 ist eine Variable i, welches den Anfangswert 9 besitzt. Diese Variable wird im folgenden eine Do-While-Schleife durchlaufen. Vorher
-I wird dabei immer um 1 subtrahiert. Die Schleife läuft solange bis i nicht mehr größer als 0 ist --> while (i > 0).
-In der Konsole wird dann alle Zahlen von 9 bis 1 ausgegeben. Dannach wird das Programm beendet.
+In der Funktion a2 durchläuft die Varibale i eine Do-While-Schleife. ES werden alle Zahlen von 9 bis 1 in der Konsole
+ausgegeben. Die Variable i verändert sich nach jeder Ausgabe, denn es wird immer um 1 subtrahiert. Die Anweisung der Schleife wird
+so lange ausgeführt bzw. wiederholt bis i nicht mehr größer als null ist (siehe Bedingung in der While Schleife).
+
 */
 function a2() {
     let i = 9;
@@ -48,20 +62,79 @@ function a2() {
 }
 a2();
 /* Aufgabe 3 - Fehler erkennen und vermeiden lernen */
-/*  a) siehe oben im Code */
-/* Aufgabe 4 - Gobal vs Lokal*/
-/*
-    a)  In der Konsole wird folgendes ausgegeben: "Hallo Bla Hallo Blubb Test Hallo" (untereinander)
-        Bei function func2 wird nicht Hallo sondern Blubb ausgegeben, weil func2 eine lokale Variable ist.
-        Hier ist die lokale Variable vorrangig und dadurch wird die globale Variable überschireben.
+/* a)
+
+function a3(): void {                       //A1
+    let x: string = "Alles";
+    console.log();
+    func4();
+    console.log("Logo!");
+}
+
+a3();
+
+function func4(): number {
+    console.log("Klar?");
+}
+
+
+
+function a3a(): void {                       //A2
+    let i = 9;
+
+    do {
+        console.log(i);
+        i = i - 1;
+        while (i > 0);
+    }
+}
+
+a2();
+
+    Ja, durch die Fehlermeldungen wird es angezeigt, was der Fehler ist.
+    Wenn man mit Maus über die Fehler drüber bewegt erscheint eine Meldung, wie man es beheben kann.
+
 */
+//b) ---> Code von eine Kommilitonin
+/*  function g1(): void {
+        let a: string = "Alles";
+        console.log(a);
+        funktion1();
+        console.log("Logo!");
+    }
+
+    g1();
+
+    function funktion2(): void {   --> Fehler! die Variablename von Funktion g1 stimmt nicht. Es muss function function1 heißen. (siehe Zeile 120)
+        console.log("Klar?");
+    }
+
+    //A2
+    function funktion2(): void {
+        console.log("Alles Klar?");
+    }
+
+    function d2(): number {        --> Fehler! Rückgabetyp ist falsch. Es musst void sein, da die Funktion nichts in der Konsole ausgibt.
+        let i: number = 9;
+
+
+        do {
+            console.log(i);
+            i = i - 1;
+        } while ( i > 0);
+    }
+
+    d2();
+
+/* Aufgabe 4 - Gobal vs Lokal*/
+//  a)  In der Konsole wird folgendes ausgegeben: "Hallo Bla Hallo Blubb Test Hallo" 
 let x = "Hallo";
 console.log(x);
 func4(x);
 console.log(x);
 func5();
 func6();
-console.log(x); /*Wieso wird diese Zeile nicht ausgegeben*/
+console.log(x); //Wieso wird diese Zeile nicht ausgegeben?
 function func4(y) {
     y = "Bla";
     console.log(y);
@@ -74,13 +147,17 @@ function func6() {
     x = "Test";
 }
 /*
-    b)  Globale Variablen sind überall sichtbar und können auch von überall zugegriffen werden.
-        Lokale Variablen existieren nur innerhalb einer Funktion.
-        Außerdem kann man eine lokale Variable deklarieren, welches die globale Variable überschreiben wird, da es vorrangig ist.
-        Jedoch gilt es nur innerhalb dessen Funktion und dannch nicht mehr.
+    b)  Die Variablen haben verschiednene Geltungsbereiche und dadurch werden sie unterschiedlich aufgerufen. Variablen
+        die außerhalb einer Funktion stehen, nennt man globale Variablen. Sie können überall im Programm aufgerufen und verwendet werden.
+        Hingegen werden lokale Variablen nur innerhalb einer Funktion deklariert und können nur innerhalb einer FUnktion aufgerufen
+        werden. Die Übergabeparameter verwendet man um eine Funktion aufzurufen. Sie werden zwischen () defniert.
+
+        Variablen dienen als Platzhalter, damit man bestimmte Werte zuweisen kann. Mit Funktionen kann man verschiedene Anweisungen
+        durchführen wie z.B. mit Kontrollstrukturen. Außerdem kann man Funktionen im Programm wiederverwenden, indem man den Namen
+        der Funktion aufruft. Sowohl Variablen als auch FUnktionen, werden mit einem Namen und Typ deklariert.
 */
 /* Aufgabe 5 - Schleifen, Funktionen und andere Kontrollstrukturen*/
-//a)
+// a)
 let xa = 8;
 let ya = 9;
 function multiply(x1, y1) {
@@ -88,7 +165,7 @@ function multiply(x1, y1) {
     return ergebnis;
 }
 console.log(multiply(xa, ya));
-//b)
+// b)
 function max(x1, y1) {
     if (x1 > y1) {
         return x1;
@@ -137,14 +214,15 @@ function leapyear(anfang, ende) {
         }
     }
 }
-//6a
+/* Aufgabe 6 - Mehr Schleifen und Funktionen */
+//a)
 let zeichen = " ";
 for (let i = 0; i < 7; i++) {
     zeichen = zeichen + "#";
     console.log(zeichen);
 }
-//6b
-for (let i = 1; i < 100; i++) {
+//b
+for (let i = 0; i <= 100; i++) {
     if (i % 3 == 0) {
         console.log("Fizz");
     }
@@ -155,4 +233,55 @@ for (let i = 1; i < 100; i++) {
         console.log(i);
     }
 }
+//c
+for (let i = 0; i <= 100; i++) {
+    if (i % 3 == 0) {
+        console.log("Fizz");
+    }
+    else if (i % 5 == 0) {
+        console.log("Buzz");
+    }
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log("Fizzbuzz");
+    }
+    else {
+        console.log(i);
+    }
+}
+//d)
+let zeichen1 = "#";
+schachbrett();
+function schachbrett() {
+    for (let i = 0; i < 8; i++) {
+        if (i % 2 == 0) {
+            console.log(" " + zeichen1 + " " + zeichen1 + " " + zeichen1 + " " + zeichen1 + "\n");
+        }
+        else {
+            console.log(zeichen1 + " " + zeichen1 + " " + zeichen1 + " " + zeichen1 + "\n");
+        }
+    }
+}
+//e) --> Leider konnte ich die Teilaufgabe e) nicht lösen. Dazu habe ich einen Ansatz. Aber bin mir nicht sicher ob es stimmt. 
+let hashtag = "#";
+let leer = " ";
+let schachbrett1 = "";
+function schachbrett2(seitenlänge) {
+    for (let i = 0; i < seitenlänge; i++) {
+        if (i % 2 == 0) {
+            for (let i = 0; i < seitenlänge / 2; i++) {
+                schachbrett1 = schachbrett1 + leer;
+                schachbrett1 += hashtag;
+            }
+        }
+        else {
+            for (let i = 0; i < seitenlänge / 2; i++) {
+                schachbrett1 += hashtag;
+                schachbrett1 = schachbrett1 + leer;
+            }
+        }
+        schachbrett1 = schachbrett1 + "\n";
+    }
+    console.log(schachbrett1);
+}
+schachbrett2(5);
 //# sourceMappingURL=script.js.map
--- Praktikumsaufgabe_P2-1/script.ts
+++ Praktikumsaufgabe_P2-1/script.ts
@@ -1,8 +1,35 @@
-/*Praktikumsaufgabe 2.1 Einführung TypeScript*/
+/* Praktikumsaufgabe 2.1 Einführung TypeScript */
 
-/* Aufgabe 1 - Basics*/
+// Aufgabe 1 - Basics
 
-/*  a)  Es wird "Alles Klar? Logo!" in der Konsole ausgegeben   */
+//  a)  In der Konsole wird "Alles Klar? Logo!" ausgegeben  
+
+/* function a$(): void {
+    let x.1: string = "Alles";
+    console.log(x);
+    func0();
+    console.log("Logo!");
+}
+
+a1();
+
+function func 0(): void {
+    console.log("Klar?");
+} 
+*/
+/*  a)  Zulässige Variablennamen können Ziffer, Buchstaben oder Sonderzeichen haben. -> z.B. a$
+        
+        Nicht zulässige Variablennamen dürfen keine Leerzeichen, Bindestrich und Punkte haben, nicht mit einem Ziffer beginnen 
+        oder als string, boolean, number, etc. heißen.
+
+    b)  Mit der Schaltfläche "Schritt darüber", wird der Debugger alle Zeile durchlaufen. Jedoch wird die innere Funktion nicht aufgerufen,
+        sondern springt zur nächste Funktion.
+
+        Mit der Schaltfläche "Schritt hinein", wird der Debugger alle Zeile nacheinander durchlaufen mit der innere Funktion.  
+
+*/
+
+//c) 
 
 function a1(): void {
     let x: string = "Alles";
@@ -27,25 +54,16 @@ function func2(): void {
 function func3(): void {
     console.log("Logo!");
 }
-/*  a)  Zulässige Variablennamen sind können Ziffer, Buchstaben oder Sonderzeichen haben. 
-        
-        Nicht zulässige Variablennamen dürfen nicht mit Ziffer beginnen, keine Leerzeichen oder Bindestrich haben 
-        oder Schlüsselwörter wie string, boolean, number etc.
 
-     b) Mit dem Schalter "Schritt darüber" werden nur die Zeile von 4-7 nacheinander durchgeführt. 
-        Jedoch überspringt es die Zeilen, indem sich die Funktion befindet
-        Mit dem Schalter "Schritt hinein" werden alle Zeilen durchlaufen. Jedoch nicht nach der Reihenfolge.
-        --> nach der Zeile 4 springt es in die Funktion a1 zur Zeile 11
 
-    c)  siehe Konsole im Browser
-*/
 
-/* Aufgabe 2 - Kontinuierliche Variablenmanipulation*/
+/* Aufgabe 2 - Kontinuierliche Variablenmanipulation */
 
 /*  
-In der FUnktion a2 ist eine Variable i, welches den Anfangswert 9 besitzt. Diese Variable wird im folgenden eine Do-While-Schleife durchlaufen. Vorher  
-I wird dabei immer um 1 subtrahiert. Die Schleife läuft solange bis i nicht mehr größer als 0 ist --> while (i > 0). 
-In der Konsole wird dann alle Zahlen von 9 bis 1 ausgegeben. Dannach wird das Programm beendet. 
+In der Funktion a2 durchläuft die Varibale i eine Do-While-Schleife. ES werden alle Zahlen von 9 bis 1 in der Konsole 
+ausgegeben. Die Variable i verändert sich nach jeder Ausgabe, denn es wird immer um 1 subtrahiert. Die Anweisung der Schleife wird 
+so lange ausgeführt bzw. wiederholt bis i nicht mehr größer als null ist (siehe Bedingung in der While Schleife).  
+
 */
 
 
@@ -64,17 +82,76 @@ a2();
 
 /* Aufgabe 3 - Fehler erkennen und vermeiden lernen */
 
-/*  a) siehe oben im Code */
+/* a)
+
+function a3(): void {                       //A1
+    let x: string = "Alles";
+    console.log();
+    func4();
+    console.log("Logo!");
+}
+
+a3();
+
+function func4(): number {
+    console.log("Klar?");
+}
 
 
+
+function a3a(): void {                       //A2
+    let i = 9;
+
+    do {
+        console.log(i);
+        i = i - 1;
+        while (i > 0);
+    }
+}
+
+a2();
+
+    Ja, durch die Fehlermeldungen wird es angezeigt, was der Fehler ist. 
+    Wenn man mit Maus über die Fehler drüber bewegt erscheint eine Meldung, wie man es beheben kann. 
+
+*/
+
+//b) ---> Code von eine Kommilitonin
+
+/*  function g1(): void {
+        let a: string = "Alles"; 
+        console.log(a);
+        funktion1(); 
+        console.log("Logo!"); 
+    }
+
+    g1();
+
+    function funktion2(): void {   --> Fehler! die Variablename von Funktion g1 stimmt nicht. Es muss function function1 heißen. (siehe Zeile 120)     
+        console.log("Klar?");
+    }
+
+    //A2
+    function funktion2(): void {
+        console.log("Alles Klar?");
+    }
+
+    function d2(): number {        --> Fehler! Rückgabetyp ist falsch. Es musst void sein, da die Funktion nichts in der Konsole ausgibt. 
+        let i: number = 9;
+
+
+        do {
+            console.log(i);
+            i = i - 1;
+        } while ( i > 0);
+    }
+
+    d2(); 
 
 /* Aufgabe 4 - Gobal vs Lokal*/
 
-/* 
-    a)  In der Konsole wird folgendes ausgegeben: "Hallo Bla Hallo Blubb Test Hallo" (untereinander)
-        Bei function func2 wird nicht Hallo sondern Blubb ausgegeben, weil func2 eine lokale Variable ist. 
-        Hier ist die lokale Variable vorrangig und dadurch wird die globale Variable überschireben. 
-*/
+//  a)  In der Konsole wird folgendes ausgegeben: "Hallo Bla Hallo Blubb Test Hallo" 
+
 
 let x: string = "Hallo";
 console.log(x);
@@ -82,7 +159,7 @@ func4(x);
 console.log(x);
 func5();
 func6();
-console.log(x); /*Wieso wird diese Zeile nicht ausgegeben*/
+console.log(x); //Wieso wird diese Zeile nicht ausgegeben?
 
 function func4(y: string): void {
     y = "Bla";
@@ -99,17 +176,20 @@ function func6(): void {
 }
 
 /*
-    b)  Globale Variablen sind überall sichtbar und können auch von überall zugegriffen werden.
-        Lokale Variablen existieren nur innerhalb einer Funktion. 
-        Außerdem kann man eine lokale Variable deklarieren, welches die globale Variable überschreiben wird, da es vorrangig ist. 
-        Jedoch gilt es nur innerhalb dessen Funktion und dannch nicht mehr.   
+    b)  Die Variablen haben verschiednene Geltungsbereiche und dadurch werden sie unterschiedlich aufgerufen. Variablen  
+        die außerhalb einer Funktion stehen, nennt man globale Variablen. Sie können überall im Programm aufgerufen und verwendet werden.
+        Hingegen werden lokale Variablen nur innerhalb einer Funktion deklariert und können nur innerhalb einer FUnktion aufgerufen 
+        werden. Die Übergabeparameter verwendet man um eine Funktion aufzurufen. Sie werden zwischen () defniert.
+
+        Variablen dienen als Platzhalter, damit man bestimmte Werte zuweisen kann. Mit Funktionen kann man verschiedene Anweisungen 
+        durchführen wie z.B. mit Kontrollstrukturen. Außerdem kann man Funktionen im Programm wiederverwenden, indem man den Namen 
+        der Funktion aufruft. Sowohl Variablen als auch FUnktionen, werden mit einem Namen und Typ deklariert. 
 */
 
 
 /* Aufgabe 5 - Schleifen, Funktionen und andere Kontrollstrukturen*/
 
-//a)
-
+// a)
 let xa: number = 8;
 let ya: number = 9;
 
@@ -121,7 +201,7 @@ function multiply(x1: number, y1: number): number {
 console.log(multiply(xa, ya));
 
 
-//b)
+// b)
 function max(x1: number, y1: number): number {
     if (x1 > y1) {
         return x1;
@@ -133,7 +213,6 @@ console.log(max(xa, ya));
 
 
 //c)
-
 let i: number = 1;
 let ergebnis: number = 0;
 while (i <= 100) {
@@ -144,8 +223,8 @@ while (i <= 100) {
 
 console.log(ergebnis);
 
-//d)
 
+//d)
 let xd: number = 0;
 let yd: number = 100;
 
@@ -156,6 +235,7 @@ function getRandomArbitrary(min: number, max: number): number {
 for (let i: number = 0; i < 10; i++) {
     console.log(getRandomArbitrary(xd, yd));
 }
+
 
 //e)
 function factorial(fakultät: number): number {
@@ -172,8 +252,8 @@ function factorial(fakultät: number): number {
 }
 console.log(factorial(7));
 
-//f)
 
+//f)
 let anfangsjahr: number = 1900;
 let endjahr: number = 2021;
 leapyear(anfangsjahr, endjahr);
@@ -188,25 +268,85 @@ function leapyear(anfang: number, ende: number): void {
 
 }
 
-//6a
+/* Aufgabe 6 - Mehr Schleifen und Funktionen */
+
+//a)
 let zeichen: string = " ";
 
-for (let i: number = 0; i < 7; i++){
+for (let i: number = 0; i < 7; i++) {
     zeichen = zeichen + "#";
     console.log(zeichen);
 }
 
-//6b
-for (let i: number = 1; i < 100; i++) {
+//b
+for (let i: number = 0; i <= 100; i++) {
     if (i % 3 == 0) {
         console.log("Fizz");
     }
     else if (i % 5 == 0) {
         console.log("Buzz");
     }
-    else{
+    else {
         console.log(i);
     }
 }
 
+//c
+for (let i: number = 0; i <= 100; i++) {
+    if (i % 3 == 0) {
+        console.log("Fizz");
+    }
+    else if (i % 5 == 0) {
+        console.log("Buzz");
+    }
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log("Fizzbuzz");
+    }
+    else {
+        console.log(i);
+    }
+}
+
+//d)
+let zeichen1: string = "#";
+schachbrett();
+
+function schachbrett(): void {
+    for (let i: number = 0; i < 8; i++) {
+        if (i % 2 == 0) {
+            console.log(" " + zeichen1 + " " + zeichen1 + " " + zeichen1 + " " + zeichen1 + "\n");
+        }
+        else {
+            console.log(zeichen1 + " " + zeichen1 + " " + zeichen1 + " " + zeichen1 + "\n");
+        }
+    }
+}
+
+//e) --> Leider konnte ich die Teilaufgabe e) nicht lösen. Dazu habe ich einen Ansatz. Aber bin mir nicht sicher ob es stimmt. 
+
+let hashtag: string = "#";
+let leer: string = " ";
+let schachbrett1: string = "";
+
+function schachbrett2(seitenlänge: number): void {
+
+    for (let i: number = 0; i < seitenlänge; i++) {
+
+        if (i % 2 == 0) {
+            for (let i: number = 0; i < seitenlänge / 2; i++) {
+                schachbrett1 = schachbrett1 + leer;
+                schachbrett1 += hashtag;
+            }
+        }
+        else {
+            for (let i: number = 0; i < seitenlänge / 2; i++) {
+                schachbrett1 += hashtag;
+                schachbrett1 = schachbrett1 + leer;
+            }
+            }
+        schachbrett1 = schachbrett1 + "\n";
+    }
+    console.log(schachbrett1);
+}
+schachbrett2(5);
 
