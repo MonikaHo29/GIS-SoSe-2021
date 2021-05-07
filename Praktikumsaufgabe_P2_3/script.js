2f18538795c789fbdae736e099012059bebe84c5
MonikaHo29 <hocamtumonika29@gmail.com>
2021-05-07 15:20:01 +0200

    Abgabe 3 bearbeitet

--- Praktikumsaufgabe_P2_3/script.js
+++ Praktikumsaufgabe_P2_3/script.js
@@ -5,18 +5,41 @@ var Praktikumsaufgabe_P2_3;
     function rectangle() {
         let div = document.createElement("div");
         document.body.appendChild(div); /* Eltern - Kindbeziehung */
-        div.style.height = (Math.random() * 400).toString() + "px";
-        div.style.width = (Math.random() * 200).toString() + "px";
-        div.style.backgroundColor = "blue";
-        div.style.position = "absolute";
+        div.style.height = (Math.random() * 600).toString() + "px";
+        div.style.width = (Math.random() * 500).toString() + "px";
+        div.style.backgroundColor = "black";
+        div.style.position = "relative";
+        div.style.marginLeft = (Math.random() * 100).toString() + "px";
+        div.style.marginTop = (Math.random() * 100).toString() + "px";
+        div.style.marginRight = (Math.random() * 100).toString() + "px";
+        div.style.marginBottom = (Math.random() * 100).toString() + "px";
     }
     for (let i = 0; i < 0; i++) {
         rectangle();
     }
-    function handleLoad(_load) {
+    function seiteZurücksetzen(_load) {
         location.reload();
     }
-    document.querySelector("#loadButton").addEventListener("click", handleLoad);
+    document.querySelector("#loadButton").addEventListener("click", seiteZurücksetzen);
     document.querySelector("#addButton").addEventListener("click", rectangle);
+    /* Aufgabe 2 */
+    //a)
+    function array(..._leerArray) {
+        let allesZusammen = [];
+        let auswahl;
+        for (let i = 0; i < _leerArray.length; i++) {
+            allesZusammen.push(auswahl[i]);
+        }
+    }
+    console.log(array);
+    let klein = create(22, 0, 0);
+    let mitte = create(0, 24, 0);
+    let groß = create(0, 0, 30);
+    function create(_durchmesserKlein, _durchmesserMitte, _durchmesserGroß) {
+        let durchmesserWählen = { durchmesserKlein: _durchmesserKlein, durchmesserMitte: _durchmesserMitte, durchmesserGroß: _durchmesserGroß };
+        return durchmesserWählen;
+    }
+    document.getSelection();
+    document.getSelection();
 })(Praktikumsaufgabe_P2_3 || (Praktikumsaufgabe_P2_3 = {}));
 //# sourceMappingURL=script.js.map
--- Praktikumsaufgabe_P2_3/script.ts
+++ Praktikumsaufgabe_P2_3/script.ts
@@ -6,21 +6,103 @@ namespace Praktikumsaufgabe_P2_3 {
 
         let div: HTMLDivElement = document.createElement("div");
         document.body.appendChild(div);                                             /* Eltern - Kindbeziehung */
-        div.style.height = (Math.random() * 400).toString() + "px";
-        div.style.width = (Math.random() * 200).toString() + "px";
-        div.style.backgroundColor = "blue";
-        div.style.position = "absolute";
+        div.style.height = (Math.random() * 600).toString() + "px";
+        div.style.width = (Math.random() * 500).toString() + "px";
+        div.style.backgroundColor = "black";
+        div.style.position = "relative";
+        div.style.marginLeft = (Math.random() * 100).toString() + "px";
+        div.style.marginTop = (Math.random() * 100).toString() + "px";
+        div.style.marginRight = (Math.random() * 100).toString() + "px";
+        div.style.marginBottom = (Math.random() * 100).toString() + "px";
+
     }
+
     for (let i: number = 0; i < 0; i++) {
         rectangle();
     }
 
-    function handleLoad(_load: Event): void {
+    function seiteZurücksetzen(_load: Event): void {
         location.reload();
     }
-    document.querySelector("#loadButton").addEventListener("click", handleLoad);
+    document.querySelector("#loadButton").addEventListener("click", seiteZurücksetzen);
+
+
     document.querySelector("#addButton").addEventListener("click", rectangle);
+
+    /* Aufgabe 2 */
+
+    //a)
+
+    function array(..._leerArray: string[]): void {   //string Rückgabe?
+
+        let allesZusammen: string[] = [];
+        let auswahl: string;
+        for (let i: number = 0; i < _leerArray.length; i++) {
+            allesZusammen.push(auswahl[i]);
+        }
+    }
+    console.log(array);
+
+
+    interface Größe {
+
+        durchmesserKlein: number;
+        durchmesserMitte: number;
+        durchmesserGroß: number;
+    }
+
+    let klein: Größe = create(22, 0, 0);
+    let mitte: Größe = create(0, 24, 0);
+    let groß: Größe = create(0, 0, 30);
+
+    function create(_durchmesserKlein: number, _durchmesserMitte: number, _durchmesserGroß: number): Größe {
+        let durchmesserWählen: Größe = {durchmesserKlein: _durchmesserKlein, durchmesserMitte: _durchmesserMitte, durchmesserGroß: _durchmesserGroß};
+        return durchmesserWählen;
+    }
+
+
+    /*interface Größe {
+
+        klein: string;
+        mittel: string;
+        groß: string;
+
+    }
+    array();
+    document.getSelection();
+    document.querySelector("#saveKlein").addEventListener("click", getSelection);
+    */
+
+
+    interface Toppings {
+
+        mozzarella: string;
+        mais: string;
+        salami: string;
+        champignions: string;
+        ruccola: string;
+
+    }
+    document.getSelection();
+
+    interface Service {
+
+        lieferung: string;
+        abholung: string;
+        vorort: string;
+
+    }
+    document.getSelection();
+
+
+
+
+
 }
+
+
+
+
 
 
 
