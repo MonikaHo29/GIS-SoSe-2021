a3bb3c8492da5aff787149323f09daa20ff0bb83
MonikaHo29 <hocamtumonika29@gmail.com>
2021-05-29 17:56:54 +0200

    Abgabe 3.1 bearbeitet

--- Praktikumsaufgabe_P3_1/server.js
+++ Praktikumsaufgabe_P3_1/server.js
@@ -5,14 +5,14 @@ const Http = require("http");
 var Praktikumsaufgabe_P3_1;
 (function (Praktikumsaufgabe_P3_1) {
     console.log("Starting server");
-    let port = Number(process.env.PORT); // Port erstellen, um eine Verbindung zum Server zu erstellen -> "Hafen"                            
-    if (!port) // If-Bedingung,    
+    let port = Number(process.env.PORT); // Port erstellen, um Anfrage zu versenden und zu erhalten -> "Hafen"                            
+    if (!port) // If-Bedingung, wenn port nicht existiert, wird es auf 8100 gestellt    
         port = 8100;
-    let server = Http.createServer(); // Server erstellen
-    server.addListener("request", handleRequest); // Listner für Abfrage und Listen + Funktionaufruf 
-    server.addListener("listening", handleListen);
+    let server = Http.createServer(); // HTTP Server erstellen
+    server.addListener("request", handleRequest); // Listener, bei einer Anfrage wird die Funktion handleRequest aufgerufen
+    server.addListener("listening", handleListen); // Listener, Bei Listen(zuhören) wird die FUnktion hnadleListen aufgerufen   
     server.listen(port);
-    // Funktion, Konsoleausgabe wird gemacht 
+    // Funktion, wenn die Funktion aufgerufen wird (Zeile 11), wird "Listening" in der KOnsole ausgegeben 
     function handleListen() {
         console.log("Listening");
     }
@@ -20,9 +20,9 @@ var Praktikumsaufgabe_P3_1;
     function handleRequest(_request, _response) {
         console.log("I hear voices!");
         _response.setHeader("content-type", "text/html; charset=utf-8");
-        _response.setHeader("Access-Control-Allow-Origin", "*");
+        _response.setHeader("Access-Control-Allow-Origin", "*"); // Wer darf auf dem Server zugreifen
         _response.write(_request.url);
-        _response.end();
+        _response.end(); // Ende, Anfrage wurde erhalten, Antwort wird versendet 
         console.log(_request.url); // Antwort (query/path string) wird in der Konsole ausgegeben
     }
 })(Praktikumsaufgabe_P3_1 = exports.Praktikumsaufgabe_P3_1 || (exports.Praktikumsaufgabe_P3_1 = {}));
--- Praktikumsaufgabe_P3_1/server.ts
+++ Praktikumsaufgabe_P3_1/server.ts
@@ -2,17 +2,17 @@ import * as Http from "http";
 
 export namespace Praktikumsaufgabe_P3_1 {                              
     console.log("Starting server");                     
-    let port: number = Number(process.env.PORT);            // Port erstellen, um eine Verbindung zum Server zu erstellen -> "Hafen"                            
-    if (!port)                                              // If-Bedingung,    
+    let port: number = Number(process.env.PORT);                        // Port erstellen, um Anfrage zu versenden und zu erhalten -> "Hafen"                            
+    if (!port)                                                          // If-Bedingung, wenn port nicht existiert, wird es auf 8100 gestellt    
         port = 8100;
 
-    let server: Http.Server = Http.createServer();          // Server erstellen
-    server.addListener("request", handleRequest);           // Listner für Abfrage und Listen + Funktionaufruf 
-    server.addListener("listening", handleListen);               
+    let server: Http.Server = Http.createServer();                      // HTTP Server erstellen
+    server.addListener("request", handleRequest);                       // Listener, bei einer Anfrage wird die Funktion handleRequest aufgerufen
+    server.addListener("listening", handleListen);                      // Listener, Bei Listen(zuhören) wird die FUnktion hnadleListen aufgerufen   
     server.listen(port);
 
 
-    // Funktion, Konsoleausgabe wird gemacht 
+    // Funktion, wenn die Funktion aufgerufen wird (Zeile 11), wird "Listening" in der KOnsole ausgegeben 
 
     function handleListen(): void {                              
         console.log("Listening");
@@ -24,11 +24,11 @@ export namespace Praktikumsaufgabe_P3_1 {
     function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
         console.log("I hear voices!");                                                                
         _response.setHeader("content-type", "text/html; charset=utf-8");    
-        _response.setHeader("Access-Control-Allow-Origin", "*");
+        _response.setHeader("Access-Control-Allow-Origin", "*");        // Wer darf auf dem Server zugreifen
         _response.write(_request.url);                                      
-        _response.end();
+        _response.end();                                                // Ende, Anfrage wurde erhalten, Antwort wird versendet 
 
-        console.log(_request.url);                          // Antwort (query/path string) wird in der Konsole ausgegeben
+        console.log(_request.url);                                      // Antwort (query/path string) wird in der Konsole ausgegeben
     }
 
 }
