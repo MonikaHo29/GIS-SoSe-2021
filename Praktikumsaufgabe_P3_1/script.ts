ae5491da4f2bf9e8e66dd98ef761e6b6fddd98d4
MonikaHo29 <hocamtumonika29@gmail.com>
2021-05-30 11:08:42 +0200

    Abgabe 3.1 bearbeitet

--- Praktikumsaufgabe_P3_1/script.ts
+++ Praktikumsaufgabe_P3_1/script.ts
@@ -1,29 +1,76 @@
 namespace Praktikumsaufgabe_P3_1 {
 
-    //FUnktion um Anfragen an Server zu schicken + Antwort (vom Server) in der Konsole 
-
-    async function sendDatatoServer(_pURL: RequestInfo): Promise<void> {
-        let response: Response = await fetch(_pURL);
-        console.log("Response", response);
-        let formData: FormData = await response.formData();
-        console.log(formData.get("fname"));
-        console.log(formData.get("femail"));
-        console.log(formData.get("fbetreff"));
-        console.log(formData.get("fdatum"));
-        console.log(formData.get("ftextfeld"));
-        console.log(formData.get("fdokument"));
-
-
-
+    async function sendData(): Promise<void> {
+        let formData: FormData = new FormData(document.forms[0]);
+        console.log(":" + formData.get("name"));
+        for (let entry of formData) {
+            console.log(entry);
+            console.log("name: " + entry[0]);
+            console.log("value: " + entry[1]);
+        }
+        // tslint:disable-next-line: no-any
+        let query: URLSearchParams = new URLSearchParams(<any>formData);
+        let _url: RequestInfo = "https://dbgissose2021.herokuapp.com/";
+        _url = _url + "?" + query.toString();
+        let answer: Response = await fetch(_url);
+        let output: string = await answer.text();
+        let displayResponse: HTMLParagraphElement = <HTMLDivElement>document.getElementById("answer");
+        displayResponse.innerText = output;
     }
-    sendDatatoServer("https://testgissose2021.herokuapp.com");
-    //Link um Daten an Server zu schicken (Heroku Deploy -> Deploy a GitHub branch
-
-    function submitButton(_load: Event): void {                                       
-    location.reload();
-    }
-    document.querySelector("#button").addEventListener("click", submitButton);
-
+    let sendButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("button");
+    sendButton.addEventListener("click", sendData);
 }
+
+
+
+
+
+
+    /*
+
+    async function sendDatatoServer(): Promise<void> {
+        let formData: FormData = new FormData(document.forms[0]);
+        for (let entry of formData) {
+            console.log(entry);
+            console.log("name: " + entry[0]);
+            console.log("value: " + entry[1]);
+        }
+
+        let url: string = "https://monikagissose2021.herokuapp.com";
+        let query: URLSearchParams = new URLSearchParams(<any>formData);
+        url = url + "?" + query.toString();
+        await fetch(url);
+    }
+    document.querySelector("#button").addEventListener("click", sendDatatoServer);
+    
+
+*/
+
+
+
+
+    //FUnktion um Anfragen an Server zu schicken + Antwort (vom Server) in der Konsole 
+    /*
+        async function sendDatatoServer(_pURL: RequestInfo): Promise<void> {
+            let response: Response = await fetch(_pURL);
+            console.log("Response", response);
+            let formData: FormData = await response.formData();
+            console.log(formData.get("fname"));
+            console.log(formData.get("femail"));
+            console.log(formData.get("fbetreff"));
+            console.log(formData.get("fdatum"));
+            console.log(formData.get("ftextfeld"));
+            console.log(formData.get("fdokument"));
+            
+        }
+        sendDatatoServer("https://monikagissose2021.herokuapp.com");
+        //Link um Daten an Server zu schicken (Heroku Deploy -> Deploy a GitHub branch
+    
+        function submitButton(_load: Event): void {                                       
+        location.reload();
+        }
+        document.querySelector("#button").addEventListener("click", submitButton);
+    */
+
 
 
