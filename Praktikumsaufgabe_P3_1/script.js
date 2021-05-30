7687404f162c7b0f4160911b59625b5df3b3748e
MonikaHo29 <hocamtumonika29@gmail.com>
2021-05-30 14:48:01 +0200

    Abgabe 3.1 bearbeitet

--- Praktikumsaufgabe_P3_1/script.js
+++ Praktikumsaufgabe_P3_1/script.js
@@ -1,23 +1,35 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Praktikumsaufgabe_P3_1 = void 0;
 var Praktikumsaufgabe_P3_1;
 (function (Praktikumsaufgabe_P3_1) {
     //Funktion um Anfragen an Server zu schicken + Antwort (vom Server) in der Konsole 
     async function sendDatatoServer(_pURL) {
         let response = await fetch(_pURL);
+        let request;
         console.log("Response", response);
         let formData = await response.formData();
-        console.log(formData.get("fname"));
-        console.log(formData.get("femail"));
-        console.log(formData.get("fbetreff"));
-        console.log(formData.get("fdatum"));
-        console.log(formData.get("ftextfeld"));
-        console.log(formData.get("fdokument"));
+        formData.get("fname");
+        formData.get("femail");
+        formData.get("fbetreff");
+        formData.get("fdatum");
+        formData.get("ftextfeld");
+        console.log(request.url);
+        /*let formData: FormData = await fetch(_pURL);
+        formData.get("fname");
+        formData.get("femail");
+        formData.get("fbetreff");
+        formData.get("fdatum");
+        formData.get("ftextfeld");
+
+        let url: string = "node Praktikumsaufgabe_P3_1/server.js";
+        let query: URLSearchParams = new URLSearchParams(formData);
+        url = url + "?" + query.toString();
+        await fetch(url);*/
     }
-    sendDatatoServer("https://monikagissose2021.herokuapp.com");
+    sendDatatoServer("node Praktikumsaufgabe_P3_1/server.js");
     //Link um Daten an Server zu schicken (Heroku Deploy -> Deploy a GitHub branch
-    function submitButton(_load) {
-        location.reload();
-    }
-    document.querySelector("#button").addEventListener("click", submitButton);
-})(Praktikumsaufgabe_P3_1 || (Praktikumsaufgabe_P3_1 = {}));
+    /*let sendButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("button");
+    sendButton.addEventListener("click", sendDatatoServer);*/
+})(Praktikumsaufgabe_P3_1 = exports.Praktikumsaufgabe_P3_1 || (exports.Praktikumsaufgabe_P3_1 = {}));
 //# sourceMappingURL=script.js.map
--- Praktikumsaufgabe_P3_1/script.ts
+++ Praktikumsaufgabe_P3_1/script.ts
@@ -1,26 +1,44 @@
-namespace Praktikumsaufgabe_P3_1 {
+import * as Http from "http";
+
+export namespace Praktikumsaufgabe_P3_1 {
 
 
     //Funktion um Anfragen an Server zu schicken + Antwort (vom Server) in der Konsole 
 
-        async function sendDatatoServer(_pURL: RequestInfo): Promise<void> {
-            let response: Response = await fetch(_pURL);
-            console.log("Response", response);
-            let formData: FormData = await response.formData();
-            console.log(formData.get("fname"));
-            console.log(formData.get("femail"));
-            console.log(formData.get("fbetreff"));
-            console.log(formData.get("fdatum"));
-            console.log(formData.get("ftextfeld"));
-            console.log(formData.get("fdokument"));
-            
-        }
-        sendDatatoServer("https://monikagissose2021.herokuapp.com");
-        //Link um Daten an Server zu schicken (Heroku Deploy -> Deploy a GitHub branch
-    
-        function submitButton(_load: Event): void {                                       
-        location.reload();
-        }
-        document.querySelector("#button").addEventListener("click", submitButton);
- 
+    async function sendDatatoServer(_pURL: RequestInfo): Promise<void> {
+        let response: Response = await fetch(_pURL);
+        let request: Http.IncomingMessage;
+        console.log("Response", response);
+        let formData: FormData = await response.formData();
+        formData.get("fname");
+        formData.get("femail");
+        formData.get("fbetreff");
+        formData.get("fdatum");
+        formData.get("ftextfeld");
+
+        console.log(request.url);
+        
+
+        /*let formData: FormData = await fetch(_pURL);
+        formData.get("fname");
+        formData.get("femail");
+        formData.get("fbetreff");
+        formData.get("fdatum");
+        formData.get("ftextfeld");
+
+        let url: string = "node Praktikumsaufgabe_P3_1/server.js";
+        let query: URLSearchParams = new URLSearchParams(formData);
+        url = url + "?" + query.toString();
+        await fetch(url);*/
+
+
     }
+    sendDatatoServer("node Praktikumsaufgabe_P3_1/server.js");
+    //Link um Daten an Server zu schicken (Heroku Deploy -> Deploy a GitHub branch
+
+    /*let sendButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("button");
+    sendButton.addEventListener("click", sendDatatoServer);*/
+
+}
+
+
