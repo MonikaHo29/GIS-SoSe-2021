a514f50d15736afcb9ef57d1d2de57a5d286afd7
MonikaHo29 <hocamtumonika29@gmail.com>
2021-06-02 16:40:07 +0200

    Abgabe 3.1 bearbeitet

--- Praktikumsaufgabe_P3_1/script.js
+++ Praktikumsaufgabe_P3_1/script.js
@@ -3,20 +3,22 @@ var Praktikumsaufgabe_3_1;
 (function (Praktikumsaufgabe_3_1) {
     //Funktion um Anfragen an Server zu schicken + Antwort (vom Server) in der Konsole
     // !Anmerkung: Funktion funktioniert nicht, ich weißt nicht wo der Fehler oder ob die Funktion so richtig ist. 
-    async function sendDatatoServer(_pURL) {
-        let response = await fetch(_pURL);
-        console.log("Response", response);
-        let formData = await response.formData();
-        console.log(formData.get("fname"));
-        console.log(formData.get("femail"));
-        console.log(formData.get("fbetreff"));
-        console.log(formData.get("fdatum"));
-        console.log(formData.get("ftextfeld"));
+    let button = document.getElementById("button"); //Button erstellen und mit der Funktion dataSenden 
+    button.addEventListener("click", dataSenden);
+    async function dataSenden() {
+        let formData = new FormData(document.forms[0]); // FormData anlegen + Formular im index 
+        console.log(":" + formData.get("fname"));
+        for (let entry of formData) { // 
+            console.log("name: " + entry[0]);
+            console.log("value: " + entry[1]);
+        }
+        let query = new URLSearchParams(formData); //
+        let url = "https://salinasapp.herokuapp.com";
+        url = url + "?" + query.toString();
+        console.log(url);
+        let response = await fetch(url);
+        let answer = await response.text();
+        console.log(answer);
     }
-    sendDatatoServer("https://monikagissose2021.herokuapp.com");
-    function submitButton(_load) {
-        location.reload();
-    }
-    document.querySelector("button").addEventListener("click", submitButton);
 })(Praktikumsaufgabe_3_1 || (Praktikumsaufgabe_3_1 = {}));
 //# sourceMappingURL=script.js.map
--- Praktikumsaufgabe_P3_1/script.ts
+++ Praktikumsaufgabe_P3_1/script.ts
@@ -1,24 +1,32 @@
- namespace Praktikumsaufgabe_3_1 {
-     
+namespace Praktikumsaufgabe_3_1 {
+
     //Funktion um Anfragen an Server zu schicken + Antwort (vom Server) in der Konsole
     // !Anmerkung: Funktion funktioniert nicht, ich weißt nicht wo der Fehler oder ob die Funktion so richtig ist. 
 
-    async function sendDatatoServer(_pURL: RequestInfo): Promise<void> {
-        let response: Response = await fetch(_pURL);
-        console.log("Response", response);
-        let formData: FormData = await response.formData();
-        console.log(formData.get("fname"));
-        console.log(formData.get("femail"));
-        console.log(formData.get("fbetreff"));
-        console.log(formData.get("fdatum"));
-        console.log(formData.get("ftextfeld"));
+    let button: HTMLButtonElement = <HTMLButtonElement>document.getElementById("button");          //Button erstellen und mit der Funktion dataSenden 
+    button.addEventListener("click", dataSenden);
+
+
+    async function dataSenden(): Promise<void> {
+
+        let formData: FormData = new FormData(document.forms[0]);                                   // FormData anlegen + Formular im index 
+        console.log(":" + formData.get("fname"));
+
+        for (let entry of formData) {                                                               // 
+
+            console.log("name: " + entry[0]);
+            console.log("value: " + entry[1]);
+        }
+        let query: URLSearchParams = new URLSearchParams(<any>formData);                            //
+        let url: RequestInfo = "https://salinasapp.herokuapp.com";
+
+        url = url + "?" + query.toString();
+        console.log(url);
+
+
+        let response: Response = await fetch(url);
+        let answer: string = await response.text();
+
+        console.log(answer);
     }
-    sendDatatoServer("https://monikagissose2021.herokuapp.com");
-
-    function submitButton(_load: Event): void {
-        location.reload();
-    }
-    document.querySelector("button").addEventListener("click", submitButton);
- }
-
-
+}
