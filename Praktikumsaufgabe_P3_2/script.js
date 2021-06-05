05d130a88a129f5147c34662cdb7555742bfe3f7
MonikaHo29 <hocamtumonika29@gmail.com>
2021-06-05 16:30:21 +0200

    Abgabe 3.2 bearbeitet

--- Praktikumsaufgabe_P3_2/script.js
+++ Praktikumsaufgabe_P3_2/script.js
@@ -9,19 +9,21 @@ var Praktikumsaufgabe_3_2;
         let formData = new FormData(document.forms[0]);
         // tslint:disable-next-line: no-any
         let query = new URLSearchParams(formData);
-        let url = "https://monikagissose2021.herokuapp.com/";
+        //let url: RequestInfo = "https://monikagissose2021.herokuapp.com/";
+        let url = "http://localhost:8100/";
         url += "/html";
         url = url + "?" + query.toString();
         let response = await fetch(url);
         let answer = await response.text();
-        let display = document.getElementById("sendDataServer");
+        let display = document.getElementById("serverantwort");
         display.innerText = answer;
     }
     async function DataJSON() {
         let formData = new FormData(document.forms[0]);
         // tslint:disable-next-line: no-any
         let query = new URLSearchParams(formData);
-        let url = "https://monikagissose2021.herokuapp.com/";
+        //let url: RequestInfo = "https://monikagissose2021.herokuapp.com/";
+        let url = "http://localhost:8100/";
         url += "/json";
         url = url + "?" + query.toString();
         let response = await fetch(url);
--- Praktikumsaufgabe_P3_2/script.ts
+++ Praktikumsaufgabe_P3_2/script.ts
@@ -18,14 +18,15 @@ namespace Praktikumsaufgabe_3_2 {
         
         // tslint:disable-next-line: no-any
         let query: URLSearchParams = new URLSearchParams(<any>formData);
-        let url: RequestInfo = "https://monikagissose2021.herokuapp.com/";
+        //let url: RequestInfo = "https://monikagissose2021.herokuapp.com/";
+        let url: RequestInfo = "http://localhost:8100/";
         url += "/html";
 
         url = url + "?" + query.toString();
 
         let response: Response = await fetch(url);
         let answer: string = await response.text();
-        let display: HTMLParagraphElement = <HTMLParagraphElement>document.getElementById("sendDataServer");
+        let display: HTMLParagraphElement = <HTMLParagraphElement>document.getElementById("serverantwort");
         display.innerText = answer;
     }
 
@@ -34,7 +35,8 @@ namespace Praktikumsaufgabe_3_2 {
 
         // tslint:disable-next-line: no-any
         let query: URLSearchParams = new URLSearchParams(<any>formData);
-        let url: RequestInfo = "https://monikagissose2021.herokuapp.com/";
+        //let url: RequestInfo = "https://monikagissose2021.herokuapp.com/";
+        let url: RequestInfo = "http://localhost:8100/";
         url += "/json";
 
         url = url + "?" + query.toString();
