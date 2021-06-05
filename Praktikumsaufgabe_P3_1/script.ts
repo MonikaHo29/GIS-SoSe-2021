bdec04dd49dc0713e1cd760310817a7d15bd8948
MonikaHo29 <hocamtumonika29@gmail.com>
2021-06-05 17:23:12 +0200

    Abgabe 3.1

--- Praktikumsaufgabe_P3_1/script.ts
+++ Praktikumsaufgabe_P3_1/script.ts
@@ -27,7 +27,7 @@ namespace Praktikumsaufgabe_3_1 {
         let antwort: string = await response.text();                     
 
         console.log(antwort);
-        let displayRueckgabe: HTMLParagraphElement = <HTMLDivElement>document.getElementById("antwort");
+        let displayRueckgabe: HTMLParagraphElement = <HTMLParagraphElement>document.getElementById("antwort");
         displayRueckgabe.innerText = antwort;
     }
 }
