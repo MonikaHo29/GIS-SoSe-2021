4b7d9004c32f708a76269620cde344ebd9fb8121
MonikaHo29 <hocamtumonika29@gmail.com>
2021-06-05 19:20:01 +0200

    Abgabe 3.1 bearbeitet

---
+++ Praktikumsaufgabe_P3_1/script.js
@@ -0,0 +1,23 @@
+"use strict";
+var Praktikumsaufgabe_3_1;
+(function (Praktikumsaufgabe_3_1) {
+    let button = document.getElementById("button");
+    button.addEventListener("click", dataSenden);
+    async function dataSenden() {
+        let formData = new FormData(document.forms[0]);
+        console.log(":" + formData.get("fname"));
+        for (let entry of formData) {
+            console.log("name: " + entry[0]);
+            console.log("value: " + entry[1]);
+        }
+        // tslint:disable-next-line: no-any
+        let query = new URLSearchParams(formData);
+        let url = "";
+        url = url + "?" + query.toString();
+        console.log(url);
+        let response = await fetch(url);
+        let answer = await response.text();
+        console.log(answer);
+    }
+})(Praktikumsaufgabe_3_1 || (Praktikumsaufgabe_3_1 = {}));
+//# sourceMappingURL=script.js.map
---
+++ Praktikumsaufgabe_P3_1/script.ts
@@ -0,0 +1,28 @@
+namespace Praktikumsaufgabe_3_1 {
+
+    let button: HTMLButtonElement = <HTMLButtonElement>document.getElementById("button");
+    button.addEventListener("click", dataSenden);
+
+    async function dataSenden(): Promise<void> {
+
+        let formData: FormData = new FormData(document.forms[0]);
+        console.log(":" + formData.get("fname"));
+
+        for (let entry of formData) {
+            console.log("name: " + entry[0]);
+            console.log("value: " + entry[1]);
+        }
+
+        // tslint:disable-next-line: no-any
+        let query: URLSearchParams = new URLSearchParams(<any>formData);
+        let url: RequestInfo = "";
+
+        url = url + "?" + query.toString();
+        console.log(url);
+
+        let response: Response = await fetch(url);
+        let answer: string = await response.text();
+        console.log(answer);
+
+    }
+}
