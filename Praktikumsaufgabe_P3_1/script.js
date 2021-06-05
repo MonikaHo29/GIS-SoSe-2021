5ebe1d116f92d4c8493f7f4af329a59015aeae69
MonikaHo29 <hocamtumonika29@gmail.com>
2021-06-05 19:47:35 +0200

    Abgabe 3.1 Verbesserung

--- Praktikumsaufgabe_P3_1/script.js
+++ Praktikumsaufgabe_P3_1/script.js
@@ -1,17 +1,18 @@
 "use strict";
 var Praktikumsaufgabe_3_1;
 (function (Praktikumsaufgabe_3_1) {
-    let button = document.getElementById("button");
+    let button = document.getElementById("button"); //Button um die Daten an den Server zu schicken
     button.addEventListener("click", dataSenden);
+    //Funktion um Daten auslesen zu können + asnycron an Server zu schicken 
     async function dataSenden() {
-        let formData = new FormData(document.forms[0]);
-        console.log(":" + formData.get("fname"));
+        let formData = new FormData(document.forms[0]); // FormData anlegen (Formular im index)
+        console.log(":" + formData.get("fname")); // Daten auslesen
         for (let entry of formData) {
             console.log("name: " + entry[0]);
             console.log("value: " + entry[1]);
         }
-        // tslint:disable-next-line: no-any
-        let query = new URLSearchParams(formData);
+        // tslint:disable-next-line: no-any                                                     // asnycron an Server versenden
+        let query = new URLSearchParams(formData); // Form Data Objekt generieren
         let url = "https://monikagissose2021.herokuapp.com/";
         url = url + "?" + query.toString();
         console.log(url);
--- Praktikumsaufgabe_P3_1/script.ts
+++ Praktikumsaufgabe_P3_1/script.ts
@@ -1,20 +1,21 @@
 namespace Praktikumsaufgabe_3_1 {
 
-    let button: HTMLButtonElement = <HTMLButtonElement>document.getElementById("button");
+    let button: HTMLButtonElement = <HTMLButtonElement>document.getElementById("button");       //Button um die Daten an den Server zu schicken
     button.addEventListener("click", dataSenden);
 
+    //Funktion um Daten auslesen zu können + asnycron an Server zu schicken 
     async function dataSenden(): Promise<void> {
 
-        let formData: FormData = new FormData(document.forms[0]);
-        console.log(":" + formData.get("fname"));
+        let formData: FormData = new FormData(document.forms[0]);                               // FormData anlegen (Formular im index)
+        console.log(":" + formData.get("fname"));                                               // Daten auslesen
 
         for (let entry of formData) {
             console.log("name: " + entry[0]);
             console.log("value: " + entry[1]);
         }
 
-        // tslint:disable-next-line: no-any
-        let query: URLSearchParams = new URLSearchParams(<any>formData);
+        // tslint:disable-next-line: no-any                                                     // asnycron an Server versenden
+        let query: URLSearchParams = new URLSearchParams(<any>formData);                        // Form Data Objekt generieren
         let url: RequestInfo = "https://monikagissose2021.herokuapp.com/";
 
         url = url + "?" + query.toString();
