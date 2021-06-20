514d65ae388a78d3483c0bdc2a9ebdbd52b6a7b1
MonikaHo29 <hocamtumonika29@gmail.com>
2021-06-20 15:35:58 +0200

    Abgabe 3.4 bearbeitet

--- Praktikumsaufgabe_P3_4_2/script.ts
+++ Praktikumsaufgabe_P3_4_2/script.ts
@@ -12,8 +12,6 @@ namespace Praktikumsaufgabe_P3_4_2 {
     buttonAbsenden.addEventListener("click", dataSenden);
     let buttonAnzeigen: HTMLButtonElement = <HTMLButtonElement>document.getElementById("buttonAnzeigen"); 
     buttonAnzeigen.addEventListener("click", dataAnzeigen);
-    //let buttonDelete: HTMLButtonElement = <HTMLButtonElement>document.getElementById("buttonDelete");
-    //buttonDelete.addEventListener("click", deleteData);
 
 
     
@@ -55,25 +53,6 @@ namespace Praktikumsaufgabe_P3_4_2 {
         let display: HTMLParagraphElement = <HTMLParagraphElement>document.getElementById("serverantwort");
         display.innerHTML = JSON.stringify(answer);
     }
-
-
-
-    /*async function deleteData(): Promise<void> {
-    
-        let formData: FormData = new FormData(document.forms[0]);
-    
-        // tslint:disable-next-line: no-any
-        let query: URLSearchParams = new URLSearchParams(<any>formData);
-        let url: RequestInfo = "https://monikagissose2021.herokuapp.com";
-        //let url: RequestInfo = "http://localhost:8100";
-    
-        url += "/delete";
-    
-        url = url + "?" + query.toString();
-        let response: Response = await fetch(url);
-        let answer: string = await response.text();
-        console.log(answer);
-    }*/
 
 
 }
